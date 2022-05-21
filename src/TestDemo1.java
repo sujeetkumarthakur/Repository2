45132f6eb40e9c70077a5e5d038c109926c16279
Ankur <ankur.wadatkar@gmail.com>
2022-05-21 08:49:23 +0530

    methods added

--- src/TestDemo1.java
+++ src/TestDemo1.java
@@ -4,7 +4,15 @@ public class TestDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiiii");
-		// line added by ankur  123456
+		// line added by ankur 123456
+
+	}
+
+	public void method1() {
+
+	}
+
+	public void method2() {
 
 	}
 
